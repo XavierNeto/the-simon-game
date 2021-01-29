39de3f4a19982d2d09f05a157188d3dedf789629
Augusto Xavier Neto <61883585+XavierNeto@users.noreply.github.com>
2021-01-29 14:24:59 -0300

    A minor correction
    It was a simple typo

--- game.js
+++ game.js
@@ -91,7 +91,7 @@ function checkAnswer(currentLevel) {
             $("body").removeClass("game-over");
         }, 200);
 
-        $("#level-title").text("Game OVer, Press Any Key to Restart");
+        $("#level-title").text("Game Over, Press Any Key to Restart");
 
         startOver();
     }
